a2517f65a62fe7ed69ddc7cb213d185014afbc0f
IpshitaZutshi <48216973+IpshitaZutshi@users.noreply.github.com>
2025-03-18 17:10:21 -0400

    updates

--- patchTask/getPatchPlaceMaps.m
+++ patchTask/getPatchPlaceMaps.m
@@ -4,8 +4,8 @@ function getPatchPlaceMaps(varargin)
 p = inputParser;
 addParameter(p,'basepath',pwd,@isstr);
 addParameter(p,'saveLoc',[],@isstr);
-addParameter(p,'forceCalculate',[],@isstr);
-addParameter(p,'plotfig',false,@islogical);
+addParameter(p,'forceCalculate',false,@isstr);
+addParameter(p,'plotfig',true,@islogical);
 
 parse(p,varargin{:});
 basepath = p.Results.basepath;
@@ -95,7 +95,7 @@ else
 
     firingMaps = bz_getRateMaps(positions,spikes,'xRange',[0 6],'yRange',[0 125], 'binSize',2.5,'saveMat',false);
     firingMaps.portRun = portRun;
-    firingMaps.portRun = rewarded;
+    firingMaps.rewarded = rewarded;
     firingMaps.highPatch = highPatch;
     firingMaps.direction = direction;
     
@@ -113,7 +113,7 @@ col1 = [238/255 67/255 69/255;...
 if plotfig
     
     plotSpikeData = 0;
-    plotTrialMaps = 0;
+    plotTrialMaps = 1;
     
     %% First plot the spike data plots
     if plotSpikeData
@@ -147,34 +147,102 @@ if plotfig
     end
     
     if plotTrialMaps
-    end
-    
-%     for pf = 1:length(firingMaps.forward.rateMaps)
-%          %  First get lin track
-%          figure
-%          set(gcf,'Renderer','painters')
-%          set(gcf,'Position',[2200 200 1185 712])
-%          for ll = 1:length(labels)
-%              for ii = 1:length(idx)
-%                 datamat = [];
-%                 numtrials = find(idx{ii}==1);
-%                 for kk = 1:length(numtrials)
-%                     datamat = [datamat;firingMaps.(labels{ll}).rateMaps{pf}{numtrials(kk)}];
-%                 end
-%                 subplot(3,13,13*(ll-1)+ii) % linear trials      
-%                 if ~isempty(datamat)
-%                     h = imagesc(datamat);
-%                     set(h, 'AlphaData', ~isnan(datamat))
-%                     
-%                 else
-%                     axis off
-%                 end
-%              end                       
-%          end                  
-% 
-%         saveas(gcf,['FiringMap',filesep ,'cell_' num2str(pf) '.png'],'png');
-%         saveas(gcf,['FiringMap',filesep ,'cell_' num2str(pf) '.fig'],'fig');
-%         close all;
-%     end    
+        % Figure out unique port combinations
+        data = unique(firingMaps.portRun, 'rows');
+
+        % Sort 
+        ascending = data(data(:,1) < data(:,2), :);
+        descending = data(data(:,1) > data(:,2), :);
+        
+        % Sort each section
+        ascending = sortrows(ascending);
+        descending = sortrows(descending, [1, 2], 'descend');
+        
+        % Concatenate ascending first, then descending
+        sorted_data = [ascending; descending];
+
+        % Stack the ratemaps according to this sorting
+        idx = cell(size(sorted_data, 1), 1);
+        idxSorted = cell(size(sorted_data, 1), 1);
+        idxHighPatch = cell(size(sorted_data, 1), 1);
+        idxLowPatch = cell(size(sorted_data, 1), 1);
+
+        highPatchLick = cell(size(sorted_data, 1), 1);
+        for ss = 1:size(sorted_data, 1)
+            idx{ss} = find(ismember(firingMaps.portRun, sorted_data(ss, :), 'rows'));
+            highPatchLick{ss} = firingMaps.highPatch(idx{ss});
+            % Sort highPatchLick descending, so that trials are separated
+            % by high versus low patch
+            [~,idxPatch] = sort(highPatchLick{ss});
+            idxSorted{ss} = idx{ss}(idxPatch);
+            idxHighPatch{ss} = idx{ss}(highPatchLick{ss}==0);
+            idxLowPatch{ss} = idx{ss}(highPatchLick{ss}==1);
+        end
+
+        numCells = length(firingMaps.rateMaps);
+
+        for cellNum = 25%1:numCells
+            sortedRateMaps{cellNum} = [];
+            sortedAvgRateMapsHighPatch{cellNum} = [];
+            sortedAvgRateMapsLowPatch{cellNum} = [];
+            for ss = 1:size(sorted_data, 1)
+                if ~isempty(idxSorted{ss})
+                    for trialNum = 1:length(idxSorted{ss})
+                        sortedRateMaps{cellNum} = [sortedRateMaps{cellNum}; firingMaps.rateMaps{cellNum}{idxSorted{ss}(trialNum)}];
+                    end
+                else
+                    ratemap = nan(1,50);
+                    sortedRateMaps{cellNum} = [sortedRateMaps{cellNum};ratemap]; 
+                end
+
+                % Make average maps
+                avgRM = [];
+                if ~isempty(idxHighPatch{ss})
+                    for trialNum = 1:length(idxHighPatch{ss})
+                        avgRM = [avgRM; firingMaps.rateMaps{cellNum}{idxHighPatch{ss}(trialNum)}];
+                    end
+                    sortedAvgRateMapsHighPatch{cellNum} = [sortedAvgRateMapsHighPatch{cellNum};nanmean(avgRM,1)];
+                else
+                    ratemap = nan(1,50);
+                    sortedAvgRateMapsHighPatch{cellNum} = [sortedAvgRateMapsHighPatch{cellNum};ratemap];
+                end
+
+                % Make average maps
+                avgRM = [];
+                if ~isempty(idxLowPatch{ss})
+                    for trialNum = 1:length(idxLowPatch{ss})
+                        avgRM = [avgRM; firingMaps.rateMaps{cellNum}{idxLowPatch{ss}(trialNum)}];
+                    end
+                    sortedAvgRateMapsLowPatch{cellNum} = [sortedAvgRateMapsLowPatch{cellNum};nanmean(avgRM,1)];
+                else
+                    ratemap = nan(1,50);
+                    sortedAvgRateMapsLowPatch{cellNum} = [sortedAvgRateMapsLowPatch{cellNum};ratemap];
+                end                
+            end
+
+            figure            
+            set(gcf,'Color','w')
+            subplot(2,2,[1 3])
+            h = imagesc(sortedRateMaps{cellNum});
+            set(h, 'AlphaData', ~isnan(sortedRateMaps{cellNum}))
+            %caxis([0 20])
+
+            subplot(2,2,2)
+            h = imagesc(sortedAvgRateMapsHighPatch{cellNum});
+            set(h, 'AlphaData', ~isnan(sortedAvgRateMapsHighPatch{cellNum}))
+            %caxis([0 20])
+
+            subplot(2,2,4)
+            h = imagesc(sortedAvgRateMapsLowPatch{cellNum});
+            set(h, 'AlphaData', ~isnan(sortedAvgRateMapsLowPatch{cellNum}))
+            %caxis([0 20])
+
+%          saveas(gcf,['FiringMap',filesep ,'cell_' num2str(cellNum) '.png'],'png');
+%          saveas(gcf,['FiringMap',filesep ,'cell_' num2str(cellNum) '.fig'],'fig');
+%          saveas(gcf,['FiringMap',filesep ,'cell_' num2str(cellNum) '.eps'],'epsc');
+%          close all;
+        end        
+
+    end  
 end
 end
